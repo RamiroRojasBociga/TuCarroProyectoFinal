34df7b8d1b4d265d6a422448ea65186f38a0e8a4
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-22 12:08:26 -0500

    se borra prueba

--- src/Principal.java
+++ src/Principal.java
@@ -2,9 +2,6 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Prueba");
-		System.out.println("Prueba2");
-		System.out.println("hola mundo");
 		
 
 	}
