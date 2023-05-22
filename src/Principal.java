8f4b4e55d9af2f862247644836028a8260b3b10e
RamiroRojasBociga <ramiro.rojasb@uqvirtual.edu.co>
2023-05-22 11:35:55 -0500

    agregado hola mundo

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Prueba");
 		System.out.println("Prueba2");
+		System.out.println("hola mundo");
+		
 
 	}
 
